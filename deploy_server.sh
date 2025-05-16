fca4c26c13fbeb1439723ae72e0863979d2255e2
ducdv79 <duc.dv@beetechsoft.com>
2025-05-16 18:34:49 +0700

    update script deploy

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,10 +1,21 @@
+echo "stop ducdv-nro-game-server"
 docker stop ducdv-nro-game-server
+echo "stop server success!!!!"
 
+echo "cd ducdv-nro-java-server"
 # shellcheck disable=SC2164
 cd ducdv-nro-java-server
+
+echo "git fetch"
 git fetch
+
+echo "git pull origin production"
 git pull origin production
+
+echo "cd .."
 # shellcheck disable=SC2103
 cd ..
 
+echo "start ducdv-nro-game-server"
 docker start ducdv-nro-game-server
+echo "start server success!!!!"
